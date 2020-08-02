ece1adf2759ed22badf3008bcaa5757d28fa0c6f
Zrisan1 <37753395+Zrisan1@users.noreply.github.com>
2020-08-01 21:29:10 -0500

    blanc?

--- src/components/barra/barra.component.jsx
+++ src/components/barra/barra.component.jsx
@@ -10,6 +10,7 @@ const Barra = () => {
               <a
                 href="https://www.facebook.com/ZoetStore-107067204436025"
                 target="_blank"
+                rel="noopener"
               >
                 <i
                   className="fab fa-facebook-square"
@@ -17,14 +18,18 @@ const Barra = () => {
                 ></i>
               </a>
 
-              <a href="https://wa.link/j85ga4" target="_blank">
+              <a href="https://wa.link/j85ga4" target="_blank" rel="noopener">
                 <i
                   className="fab fa-whatsapp-square"
                   style={{ fontSize: "38px", marginRight: "40px" }}
                 ></i>
               </a>
 
-              <a href="https://www.instagram.com/zoetstore/" target="_blank">
+              <a
+                href="https://www.instagram.com/zoetstore/"
+                target="_blank"
+                rel="noopener"
+              >
                 <i
                   className="fab fa-instagram-square"
                   style={{ fontSize: "38px" }}
